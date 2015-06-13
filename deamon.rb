a566594b895796eee2eada645acdd7691fcb720e
Stephen Best <bestie@gmail.com>
2015-06-13 17:11:40 +0100

    Refactor and rescue EOF

--- deamon.rb
+++ deamon.rb
@@ -3,15 +3,20 @@ require "socket"
 
 socket_path = "socket"
 
-serv = UNIXServer.new(socket_path)
+server = UNIXServer.new(socket_path)
 
 loop do
-  s = serv.accept
-  input = s.readline
-  puts input
-  if input == "exit\n"
-    puts "Got exit"
-    s.close
-    break
+  begin
+    socket = server.accept
+    input = socket.readline
+    puts input
+    if input == "exit\n"
+      puts "Got exit"
+      socket.close
+      break
+    end
+  rescue EOFError => e
   end
 end
+
+server.close
